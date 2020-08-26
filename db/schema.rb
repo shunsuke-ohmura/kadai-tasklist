20a477402389358fdf9fc7e8f3bc683f255f391d
shunsuke.ohmura <restart05201179@yahoo.co.jp>
2020-08-26 15:00:44 +0000

    CRUD tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_085123) do
+ActiveRecord::Schema.define(version: 2020_08_26_144439) do
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
