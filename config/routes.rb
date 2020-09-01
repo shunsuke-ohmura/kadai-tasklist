ee56317ebf190f7f94fca8d8d79579082e58d2b0
shunsuke.ohmura <restart05201179@yahoo.co.jp>
2020-09-01 17:53:55 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
     root to: 'tasks#index'
     
-    
     get 'signup', to: 'users#new'
+    resources :toppages, only: [:index]
+    resources :users, only: [:index, :show, :new, :create]
     resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
